7d72d28c1b5cde7a41ec9c1063ce9900ad9fe37d
andras@newzoo.com <andras@newzoo.com>
2020-12-20 11:19:42 +0100

    added redocrderReducer to root reducer

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,8 +1,11 @@
 import React from 'react';
+
+import Recorder from '../Recoder/Recorder';
+
 import './App.css';
 
 const App: React.FC = () => {
-  return <div className="App">Hello</div>;
+  return <div className="App"><Recorder/></div>;
 };
 
 export default App;
